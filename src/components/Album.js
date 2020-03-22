8a9b8a0e72319f29dc0ff582093154c693176882
Val <valeryharisoanitolotra@gmail.com>
2020-03-22 17:36:22 +0100

    mettre en rouge le icon MUSIC pour que mr Buffa peut le voir

--- src/components/Album.js
+++ src/components/Album.js
@@ -8,7 +8,7 @@ import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 import TableContainer from "@material-ui/core/TableContainer";
 import Paper from "@material-ui/core/Paper";
 import Table from "@material-ui/core/Table";
@@ -51,8 +51,8 @@ const Album = ({album}) => {
                         </Avatar>
                     }
                     action={
-                        <IconButton aria-label="settings" onClick={handleClickOpen}>
-                            <MoreVertIcon/>
+                        <IconButton aria-label="settings" onClick={handleClickOpen} className="Album-song">
+                            <LibraryMusicIcon/>
                         </IconButton>
                     }
                     title={album.title}
